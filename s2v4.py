dd5148317c9055baf21915b67e8c13c80d6b0593
Alex Miles Younger <alex@alexmilesyounger.com>
2015-06-21 10:28:52 -0400

    Documentation on default arguments for the find_average function

--- s2v4.py
+++ s2v4.py
@@ -1,11 +1,12 @@
 from s2v3 import *
 
-def find_average(data_sample):
+# Find the average
+def find_average(data_sample, header=False): #update the function to avoid having to use slices to skip over header rows
 	total = calculate_sum(data_sample)
 	size = number_of_records(data_sample)
 	average = total / size
 	return average
 
-average_price = find_average(data_from_csv[1:]) # here we go slicing our way past the header row again, I just think that's a stupid way to handle this. I feel like we shoul always munge the data so that headers are automatically not included. See what I did with munge there? Yeah, I'm getting into the terminology
+average_price = find_average(data_from_csv, True) # Second argument takes a boolean value that indicates if the data_sample has headers. Above we've set the default value to False (which makes this an optional argument), but if we set it to True it means we want to skip the first row. Now, see I think I'd just set the default to True so that the function always skipped the first row (the headers). It's less complicated that way. Most of the time we're going to want to work with the non-header values, not the headers, so I feel like we should make getting at the headers a non-default option and keep the majority cases as the defualt.
 
-print("Average is", average_price) # no need to add spaces because "," seems to automatically add one (at least in the print function)
+print("Average =", average_price) # no need to add spaces because "," seems to automatically add one (at least in the print function)
