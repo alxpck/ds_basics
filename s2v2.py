428889029541bb5c8f8998eb1f4cbc057a80fb87
Alex Miles Younger <alex@alexmilesyounger.com>
2015-06-21 09:19:44 -0400

    Subtract header row for acurate counting

--- s2v2.py
+++ s2v2.py
@@ -3,6 +3,6 @@ from s2v1 import *
 def number_of_records(data_sample):
 	return len(data_sample)
 
-number_of_ties = number_of_records(data_from_csv)
+number_of_ties = number_of_records(data_from_csv) - 1 # minus header row
 
 print(number_of_ties, "ties in our data sample")
