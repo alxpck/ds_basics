5547e59360126baa20e1684a22e7f88fdacb530a
Alex Miles Younger <alex@alexmilesyounger.com>
2015-06-22 12:54:27 -0400

    Create new function for number of records and do a header check

--- s2v2.py
+++ s2v2.py
@@ -3,10 +3,15 @@ from s2v1 import *
 def number_of_records(data_sample):
 	return len(data_sample)
 
+def number_of_records_ignore_header(data_sample, header=True):
+	if header: 
+		return len(data_sample) - 1
+	else: 
+		return len(data_sample)
+
 number_of_ties = number_of_records(data_from_csv) - 1 # minus header row
 
 # print(number_of_ties, "ties in our data sample")
-
 def number_of_records2(data_sample):
 	return data_sample.size
 
